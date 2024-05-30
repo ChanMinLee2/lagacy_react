d6b6b6a043a30b78c48d8247980da14869e646d1
chanmin Lee <dlcksals87@gmail.com>
2024-05-30 17:50:57 +0900

    🎨 FEAT : 로그인 구현

--- my_legacy/src/Pages/login/LoginPage.jsx
+++ my_legacy/src/Pages/login/LoginPage.jsx
@@ -1,19 +1,49 @@
 // LoginPage.jsx
 import React from "react";
 import * as S from "./LoginPage.style";
+import { useState } from "react";
+import axios from "axios";
+import { url, config } from "../../api/apiConstants";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const handleSubmit = (e) => {
+  const [loginID, setLoginID] = useState("");
+  const [loginPW, setLoginPW] = useState("");
+  const nav = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // 로그인 처리 로직 추가
+    const data = { username: loginID, password: loginPW };
+    const loginURL = url + "/user/login";
+
+    try {
+      const res = await axios.post(loginURL, data, config);
+      console.log(res.data);
+      nav("/menu");
+    } catch (error) {
+      console.error("Login error:", error);
+    }
   };
+  console.log(loginID, loginPW);
 
   return (
     <S.Container>
       <S.Form onSubmit={handleSubmit}>
         <S.Title>로그인</S.Title>
-        <S.Input type="text" placeholder="사용자 명" required />
-        <S.Input type="password" placeholder="비밀번호" required />
+        <S.Input
+          type="text"
+          placeholder="사용자명"
+          required
+          defaultValue={loginID}
+          onChange={(e) => setLoginID(e.target.value)}
+        />
+        <S.Input
+          type="password"
+          placeholder="비밀번호"
+          required
+          defaultValue={loginPW}
+          onChange={(e) => setLoginPW(e.target.value)}
+        />
 
         <S.Footer>
           <S.Links to="/signup">계정 만들기</S.Links>
---
+++ my_legacy/src/api/apiConstants.js
@@ -0,0 +1,10 @@
+const url = "http://172.30.1.22:8000";
+// 설정 객체
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer your-token-here",
+  },
+};
+
+export { url, config };
